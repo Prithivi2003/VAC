1476e58d32135f791fbe2b58e274292e7c6d30cc
Prithivi2003 <prithivirajacm@gmail.com>
2023-07-25 13:50:09 +0530

    all

---
+++ cm.js
@@ -0,0 +1,16 @@
+console.log("Hello..World..!");
+
+//single line command
+/*
+multilne
+command
+*/
+
+//variable
+let name = "Prithivi..!";
+console.log(name);
+name = "Rajan";
+console.log(name);
+
+const age = 20;
+console.log(age);
